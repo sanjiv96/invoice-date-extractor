c3bedcd5fe558fbddf9c9672749e0a77ab4deaca
sanjiv96 <36184583+sanjiv96@users.noreply.github.com>
2019-12-01 14:14:13 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author Sanjeev
+The client file to encode image to base64 format and make post request to the 
+Rest server endpoint  
+"""
+import requests
+import base64
+url = 'http://52.27.172.222:5000/extract_date'
+image_path = "Add full path of image here"
+with open(image_path, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    myobj = {"base_64_image_content": encoded_string}       
+    x = requests.post(url, json=myobj)
+    print(x.content)     
